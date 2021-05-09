1d264b2258ae78bee8c2ee054a38af36dc520ff7
basantloay <basantloay2012@gmail.com>
2021-05-09 17:44:45 +0200

    Add main class

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,25 @@
 package com.company;
-
+import java.io.File;
+import java.io.FileNotFoundException;  // Import this class to handle errors
+import java.util.Queue;
+import java.util.LinkedList;
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Queue<Website> q= new LinkedList<>();
+        try {
+            File file = new File("websites.txt");
+            Scanner scannedFile = new Scanner(file);
+            while (scannedFile.hasNextLine()) {
+                String URL = scannedFile.nextLine();
+                q.add(new Website(URL));
+                System.out.println(URL);
+            }
+            scannedFile.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
     }
 }
