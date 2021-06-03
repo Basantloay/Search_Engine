92a4828e6bf1a4de0a18c3570fc04326dfb3113f
basantloay <basantloay2012@gmail.com>
2021-06-03 13:25:50 +0200

    Modify robots.text

--- src/com/company/Crawler/Crawler.java
+++ src/com/company/Crawler/Crawler.java
@@ -8,16 +8,15 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import java.io.*;
-import java.net.UnknownHostException;
+import java.net.*;
 import java.nio.file.FileSystems;
 import java.nio.file.NoSuchFileException;
 import java.nio.file.Path;
 import java.util.concurrent.atomic.AtomicInteger;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.nio.channels.Channels;
 import java.nio.channels.ReadableByteChannel;
 import java.util.*;
+import java.util.regex.*;
 
 @SuppressWarnings("ALL")
 public class Crawler implements Runnable{
@@ -44,65 +43,55 @@ public class Crawler implements Runnable{
         this.allowed = allowed;
     }
 
-    public boolean robots(String args, Integer num) throws IOException {
+    public boolean robots(String args, Integer num) throws IOException, URISyntaxException {
         boolean cont=false;boolean find=false;
         if (args=="")
             return false;
 
         URL w = new URL(args + "/robots.txt");
+        if (w.getHost() == null)
+            return false;
+         w.toURI();
         try(BufferedReader in = new BufferedReader(new InputStreamReader(w.openStream()))) {
             String line = null;
+            int index;
+            String website=args;
             while((line = in.readLine()) != null) {
                 //System.out.println(line);
                 if (line.contains("<!DOCTYPE html>")) {
                    // System.out.println("\nl2naha");
                     return false;
                 }
-                else
-                {
-                    if(line.contains("User-agent"))
-                    {
-                        if(line.contains(name))
-                            cont=true;
+                else {
+                    if (line.contains("User-agent")) {
+                        if (line.contains(name))
+                            cont = true;
                         if (line.contains("*")) {
                             cont = true;
                         }
-                        find=true;
-                    }
-                    else if(find && cont)
-                    {
-                        if(line.contains("Disallow"))
-                        {
-                            int index=line.length()-1;
-                            if(line.contains("*"))
-                              index=line.indexOf("*");
-                            //10 b3d disallow
-                            if(line.length()>11)
-                            {
-                            synchronized (disallowed){
+                        find = true;
+                    } else if (find && cont) {
+                        if (line.contains("Disallow")) {
+                            if(line.length()<10)
+                                return false;
+                            website += (line.substring(10) );
+                            website=website.replace("*",".*");
+                            website=website.replace("?","\\?");
+                            website=website.replace("+","\\+");
+                            website=website.replaceAll(".","\\.");
+                                    //System.out.println(args + (line.substring(10,index)+line.substring(index+1)));
+                                    synchronized (disallowed) {
 
-                            disallowed.add(args + (line.substring(10,index)+line.substring(index+1)));}
-                            //System.out.println(args + (line.substring(10,index)+line.substring(index+1)));
-                            }
-                        }
-                        else if(line.contains("Allow"))
-                        {
-                            //7 b3d allow
-                            int index=line.length()-1;
-                            if(line.contains("*"))
-                                index=line.indexOf("*");
-                            if(line.length()>8)
-                            {
-                                synchronized (allowed){
-
-                                    allowed.add(args + (line.substring(7,index)+line.substring(index+1)));}
-                                //System.out.println(args + (line.substring(7,index)+line.substring(index+1)));
+                                        disallowed.add(website );
+                                    }
                                 }
-                        }
 
+
+                            }
+
+                        }
                     }
-                }
-            }
+
             return true;
 
         } catch (FileNotFoundException e ) {
@@ -119,6 +108,7 @@ public class Crawler implements Runnable{
 
     Boolean flag =true;
     public void parse(String args) throws IOException {
+
         FileWriter f1=new FileWriter("Test.txt");
         int j=0;
 
@@ -148,8 +138,9 @@ public class Crawler implements Runnable{
             System.out.println("Error in file");
         }
         Integer i = 0;
-        while (!seedSet.isEmpty() || crawlerCount.intValue() < max) {
-            try{
+        HttpURLConnection con;
+        while (!seedSet.isEmpty() && crawlerCount.intValue() <= max) {
+            try {
                 crawlerCount.incrementAndGet();
                 System.out.println(Thread.currentThread().getName());
                 //System.out.println(crawlerCount);
@@ -162,63 +153,86 @@ public class Crawler implements Runnable{
 
                 }
 
-                f1.write(website + '\n');
+
                 if (website != null || website.length() != 0)
                     robots(website, i);
                 int timeout;
                 //final Connection.Response postResponse = Jsoup.connect(website).execute();
                 //if (Jsoup.connect(website).execute()!=null) {
                 //System.out.println("a");
-                if (website != null || website.length() != 0)
-                {
-                    Document doc = Jsoup.connect(website).userAgent("Mozilla/5.0 (Windows NT 6.0) AppleWebKit/535.2 (KHTML, like Gecko) Chrome/15.0.874.121 Safari/535.2").followRedirects(true).method(Connection.Method.POST).timeout(200000).ignoreHttpErrors(true).get();
-                //Document doc = Jsoup.parseBodyFragment(website);
 
-                //System.out.println(doc);
-                    Elements links = doc.select("a[href]");
+                if (website != null && website.length() != 0) {
+                    URL w = new URL(website);
+                     con = (HttpURLConnection) w.openConnection();
 
-                i++;
-                boolean flag1, flag2;
-                for (Element link : links) {
-                    String str = link.attr("abs:href");
-                    synchronized (seedSetVisited) {
-                        flag1 = seedSetVisited.contains(str);
-                    }
-                    synchronized (seedSet) {
-                        flag2 = seedSet.contains(str);
-                    }
-                    synchronized (disallowed) {
-                        if (!flag1 && !flag2 && !disallowed.contains(str)) {
+                    try {
+                        if (w.getHost() != null) {
+                        con.getResponseCode();
+
+                            Document doc = Jsoup.connect(website).userAgent("Mozilla/5.0 (Windows NT 6.0) AppleWebKit/535.2 (KHTML, like Gecko) Chrome/15.0.874.121 Safari/535.2").followRedirects(true).method(Connection.Method.GET).timeout(1200000).ignoreHttpErrors(true).get();
+                            //Document doc = Jsoup.parseBodyFragment(website);
+                            f1.write(website + '\n');
+                            //System.out.println(doc);
+                            Elements links = doc.select("a[href]");
+
+                            i++;
+                            boolean flag1, flag2;
+                            for (Element link : links) {
+                                String str = link.attr("abs:href");
+                                synchronized (seedSetVisited) {
+                                    flag1 = seedSetVisited.contains(str);
+                                }
+                                synchronized (seedSet) {
+                                    flag2 = seedSet.contains(str);
+                                }
+                                int iter = 0;
+                                boolean flag3 = false;
+                                synchronized (disallowed) {
+                                    while (iter < disallowed.size()) {
+                                        if (Pattern.matches(disallowed.get(iter), str)) {
+                                            flag3 = true;
+                                            break;
+                                        }
+                                        iter++;
+                                    }
+                                }
+                                if (!flag1 && !flag2 && !flag3) {
+                                    synchronized (seedSet) {
+                                        seedSet.add(str);
+                                    }
+                                }
+
+                                //System.out.println("\nlink:" + str);
+
+
+                            }
+                            synchronized (seedSetVisited) {
+                                seedSetVisited.add(website);
+                            }
                             synchronized (seedSet) {
-                                seedSet.add(str);
+                                seedSet.remove(website);
                             }
                         }
-                    }
-                    //System.out.println("\nlink:" + str);
 
-
+                    }catch (UnknownHostException e) {
+                        System.out.println("Unknown Host ");
+                        con.disconnect();
                 }
-                synchronized (seedSetVisited) {
-                    seedSetVisited.add(website);
-                }
-                synchronized (seedSet) {
-                    seedSet.remove(website);
-                }
-                }
-                //}
-            } catch(FileNotFoundException e){
-                e.printStackTrace();
-                System.out.println("Error in file");
+            }
+            }
+                catch(FileNotFoundException e){
+                //e.printStackTrace();
+                System.out.println("File is not found");
                 System.out.println(crawlerCount);
             } catch(IOException e){
-                e.printStackTrace();
-                System.out.println("Error");
+                //e.printStackTrace();
+                System.out.println("Invalid");
                 System.out.println(crawlerCount);
             }
-        catch(IllegalArgumentException x)
+        catch(IllegalArgumentException | URISyntaxException x)
             {
-                x.printStackTrace();
-                System.out.println("error");
+                //x.printStackTrace();
+                System.out.println("Inavalid URL syntax");
                 System.out.println(crawlerCount);
             }
 
